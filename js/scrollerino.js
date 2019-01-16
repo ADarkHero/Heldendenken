4095d6f8ba20d5632d0d40bb28e69262dfcd29cb
ADarkHero <maik.riedlsperger@gmail.com>
2019-01-16 09:15:34 +0100

    Scrolling fix for uneven amounts of cards.

--- js/scrollerino.js
+++ js/scrollerino.js
@@ -3,17 +3,20 @@ var lastscroll = 0;
 $(window).scroll(function() {
     var sleft = $(window).scrollLeft(); //Where is the scroll-bar currently?
     var swidth = $(window).width(); //How big is the users window?
+    var maxscroll = window.scrollMaxX || (document.documentElement.scrollWidth - document.documentElement.clientWidth);
     var news = 0;
-        
-    if(sleft % swidth !== 0 || sleft === 0){ //Prevents the event from triggering itself
+    
+    console.log("sleft " + sleft + " swidth " + swidth + " lastscroll " + lastscroll + " maxscroll " + maxscroll);
+    
+    if((sleft % swidth !== 0 && sleft !== maxscroll) || sleft === 0){ //Prevents the event from triggering itself
         if(lastscroll < sleft){ //Scroll right
             news = swidth * (Math.floor(sleft/swidth) + 1);
         }
         else if(lastscroll > sleft){ //Scroll left
             news = swidth * (Math.floor(sleft/swidth));  
         }
-        console.log(" New Scroll: "+news);
         $(window).scrollLeft(news);
         lastscroll = news;
     }
+    
 });
